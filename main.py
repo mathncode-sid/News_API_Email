72e2e83909f5fbb8d9c2dc03b45d916dc867bf22
mathncode-sid <sidneymuriuki1@gmail.com>
2025-04-08 01:32:01 +0300

    Add links, specify language, limit news, added subject, control category, dynamic input

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
 from send_mail import send_mail
+
+topic = input("Enter your topic: ")
 api_key = '1956a9df3d884c4a9f5fb06d920f7aac'
-url = ("https://newsapi.org/v2/everything?q=n8n&"
-       "from=2025-03-07&sortBy=publishedAt&"
-       "apiKey=1956a9df3d884c4a9f5fb06d920f7aac")
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "from=2025-03-07&"
+       "sortBy=publishedAt&"
+       "apiKey=1956a9df3d884c4a9f5fb06d920f7aac"
+       "&language=en")
 
 #Make request
 request = requests.get(url)
@@ -12,10 +17,11 @@ request = requests.get(url)
 content = request.json()
 
 #Access the article titles and description
-body = ""
-for article in content['articles']:
-    if article['title'] is not None:
-        body = body + article['title'] + "\n" + article['description'] + "\n" + 2*"\n"
+body = "Subject: Today's news \n\n"
+
+for article in content['articles'][:20]:
+    if article.get('title') and article.get('description') and article.get('url'):
+        body += f"{article['title']}\n\n {article['description']}\n{article['url']}\n\n"
 
 body = body.encode('utf-8')
 send_mail(message=body)
