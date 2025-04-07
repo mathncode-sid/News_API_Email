a00c94851406f1ef252ce48fdd62b8e3bfb2094a
mathncode-sid <sidneymuriuki1@gmail.com>
2025-04-08 01:09:14 +0300

    Sent emails with news titles and descriptions

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
-from certifi import contents
-
+from send_mail import send_mail
 api_key = '1956a9df3d884c4a9f5fb06d920f7aac'
 url = ("https://newsapi.org/v2/everything?q=n8n&"
        "from=2025-03-07&sortBy=publishedAt&"
@@ -13,6 +12,11 @@ request = requests.get(url)
 content = request.json()
 
 #Access the article titles and description
+body = ""
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    if article['title'] is not None:
+        body = body + article['title'] + "\n" + article['description'] + "\n" + 2*"\n"
+
+body = body.encode('utf-8')
+send_mail(message=body)
+
